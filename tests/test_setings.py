98ba071c9257dc9d1e7477c51b404b73709e6e91
V_A_N <V_A_N-1_94@mail.ru>
2023-12-11 18:31:06 +0300

    fixed code

--- tests/test_setings.py
+++ tests/test_setings.py
@@ -5,5 +5,5 @@ from src.setings import OPERATION_PATH
 
 def test_path():
     path_ = Path(__file__).parent.parent
-    expected_path = path_.joinpath('data', 'operations.json')
+    expected_path = path_.joinpath("data", "operations.json")
     assert OPERATION_PATH == expected_path
